cd987f7adcb65103f381caf4ac9cb9e378ef5a21
Li-Hung Chen <badstyle319@hotmail.com>
2020-05-18 00:57:10 +0800

    1. template files added

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+#ifdef DBG
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+	// clock_t begin = clock();
+#endif
+	
+    
+	
+#ifdef DBG
+	// clock_t end = clock();
+	// double elapsed_secs = double(end-begin)/CLOCKS_PER_SEC;
+	// printf("elapsed: %.3lf seconds\n", elapsed_secs);
+	fclose(stdin);
+	fclose(stdout);
+#endif 
+
+	return 0;
+}
